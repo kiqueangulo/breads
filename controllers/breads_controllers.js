f250e35d73bb0cac32d8d5c18848e2df35a61329
kiqueangulo <kiqueangulo@gmail.com>
2022-05-07 11:47:53 -0400

    Adding data

--- controllers/breads_controllers.js
+++ controllers/breads_controllers.js
@@ -3,8 +3,13 @@ const breads = express.Router();
 const Bread = require('../models/breads.js');
 
 // Index
-breads.get('/:arrayIndex', (req, res) => {
-    res.send(Bread[req.params.arrayIndex]);
+breads.get('/', (req, res) => {
+    res.render('Index', 
+        {
+            breads: Bread
+        }
+    );
+    // res.send(Bread[req.params.arrayIndex]);
 });
 
 module.exports = breads;
