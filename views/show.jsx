eab1ec985a28a4908c331cc4e5c086a858f7e740
kiqueangulo <kiqueangulo@gmail.com>
2022-05-09 18:27:42 -0400

    Adding data & conditionally rendering HTML based on data

--- views/show.jsx
+++ views/show.jsx
@@ -2,10 +2,20 @@ const React = require('react');
 const Default = require('./layouts/Default');
 
 function Show ({bread}) {
-  console.log(bread.name)
     return (
       <Default>
-        <h2>Show Page</h2>
+        <h3>{bread.name}</h3>
+        <p>
+            and it 
+            {
+                bread.hasGluten
+                ? <span> has </span>
+                : <span> does NOT have </span>
+            }
+            gluten.
+        </p>
+        <img src={bread.image} alt={bread.name} />
+        <li><a href="/breads">Go Home</a></li>
       </Default>
     )
 };
