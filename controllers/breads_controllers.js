6f7787d2e4e372830ae1ee89d17288f530a36c32
kiqueangulo <kiqueangulo@gmail.com>
2022-05-11 19:42:52 -0400

    Edit route created

--- controllers/breads_controllers.js
+++ controllers/breads_controllers.js
@@ -18,6 +18,26 @@ breads.get('/new', (req, res) => {
     res.render('new')
 });
 
+// Edit
+breads.get('/:indexArray/edit', (req, res) => {
+    res.render('edit', {
+        bread: Bread[req.params.indexArray],
+        index: req.params.indexArray
+    })
+});
+
+// Update
+breads.put('/:arrayIndex', (req, res) => {
+    if (req.body.hasGluten === 'on') {
+        req.body.hasGluten = true
+    } else {
+        req.body.hasGluten = false
+    };
+
+    Bread[req.params.arrayIndex] = req.body;
+    res.redirect(`/breads/${req.params.arrayIndex}`);
+});
+
 // Show
 breads.get('/:arrayIndex', (req, res) => {
     if (Bread[req.params.arrayIndex]) {
@@ -43,18 +63,6 @@ breads.post('/', (req, res) => {
 
     Bread.push(req.body);
     res.redirect('/breads'); // It takes the user back the index page
-});
-
-// Update
-breads.put('/:arrayIndex', (req, res) => {
-    if (req.body.hasGluten === 'on') {
-        req.body.hasGluten = true
-    } else {
-        req.body.hasGluten = false
-    };
-
-    Bread[req.params.arrayIndex] = req.body;
-    res.redirect(`/breads/${req.params.arrayIndex}`);
 });
 
 // Delete
