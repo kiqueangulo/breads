b76380cdadc4c82acdf61f00598feba9a1fd14b6
kiqueangulo <kiqueangulo@gmail.com>
2022-05-16 19:30:17 -0400

    Setting up the DB

--- models/breads.js
+++ models/breads.js
@@ -1,62 +1,15 @@
-module.exports = [
-    {
-        name: 'Rye',
-        hasGluten: true,
-        ingredients: [
-            'Active dry yeast',
-            'Molasses (regular, NOT blackstrap)',
-            'Salt',
-            'Vegetable oil',
-            'Cocoa powder (unsweetened)',
-            'Rey flour',
-            'Bread flour',
-            'Caraway seeds (optional)'
-        ],
-        image: 'https://images.unsplash.com/photo-1595535873420-a599195b3f4a?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80',
-    },
-    {
-        name: 'French',
-        hasGluten: true,
-        ingredients: [
-            'Sugar',
-            'Instant or active dry yeast',
-            'Salt',
-            'Olive oil, canola oil, vegetable oil or avocado oil',
-            'All purpose flour or bread flour'
-        ],
-        image: 'https://images.unsplash.com/photo-1534620808146-d33bb39128b2?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=634&q=80',
-    },
-    {
-        name: 'Gluten-Free',
-        hasGluten: false,
-        ingredients: [
-            'Active dried yeast',
-            'Sugar',
-            'Psylliu, husk',
-            'Potato starch',
-            'Brown rice flour',
-            'Buckwheat flour',
-            'Salt',
-            'Apple cider vinegar'
-        ],
-        image: 'https://images.unsplash.com/photo-1546538490-0fe0a8eba4e6?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=1050&q=80',
-    },
-    {
-        name: 'Pumpernickel',
-        hasGluten: true,
-        ingredients: [
-            'Bread flour',
-            'Dark rey flour',
-            'Cornmeal',
-            'Whole-wheat flour',
-            'Unsweetened cocoa powder',
-            'Kosher salt',
-            'Milk',
-            'Dark brown sugar',
-            'Active dry yeast',
-            'Unsulphured molasses',
-            'Unsalted butter'
-        ],
-        image: 'https://images.unsplash.com/photo-1586444248902-2f64eddc13df?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=1050&q=80',
-    }
-];
+// require mongoose 
+const mongoose = require('mongoose')
+// creating shorthand for the Schema constructor 
+const { Schema } = mongoose 
+
+// Schema
+const breadSchema = new Schema({
+    name: { type: String, required: true },
+    hasGluten: Boolean,
+    image: { type: String, default: 'http://placehold.it/500x500.png' }
+})
+
+// Model and export
+const Bread = mongoose.model('Bread', breadSchema)
+module.exports = Bread
