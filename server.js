bc0fef9dfdda767c72247fe1e9b163b040fed71d
kiqueangulo <kiqueangulo@gmail.com>
2022-05-16 18:54:56 -0400

    Integrating MongoDB

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 // Dependencies
 const express = require('express');
+const mongoose = require('mongoose');
 const methodOverride = require('method-override');
 
 // Configuration
@@ -11,13 +12,14 @@ const app = express();
 app.set('views', __dirname + '/views');
 app.set('view engine', 'jsx');
 app.engine('jsx', require('express-react-views').createEngine());
-
 app.use(express.static('public'));
-
 app.use(express.urlencoded({extended: true}));
-
 app.use(methodOverride('_method')); // To override the POST method at show.jsx
 
+// MongoDB integration
+mongoose.connect(process.env.MONGO_URI, {useNewUrlParser: true, useUnifiedTopology: true},
+    () => { console.log('connected to mongo: ', process.env.MONGO_URI) }    
+);
 
 // Routes
 app.get('/', (req, res) => {
