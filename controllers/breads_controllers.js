3b183a31b3b7a07e3f19e8eb01f5dd84b365ffcd
kiqueangulo <kiqueangulo@gmail.com>
2022-05-18 18:47:25 -0400

    Reorganizing routes

--- controllers/breads_controllers.js
+++ controllers/breads_controllers.js
@@ -15,44 +15,6 @@ breads.get('/', (req, res) => {
         })
 });
 
-// New
-breads.get('/new', (req, res) => {
-    res.render('new')
-});
-
-// Update
-breads.put('/:arrayIndex', (req, res) => {
-    if (req.body.hasGluten === 'on') {
-        req.body.hasGluten = true
-    } else {
-        req.body.hasGluten = false
-    };
-
-    Bread[req.params.arrayIndex] = req.body;
-    res.redirect(`/breads/${req.params.arrayIndex}`);
-});
-
-// Show
-breads.get('/:id', (req, res) => {
-    Bread.findById(req.params.id)
-        .then(foundBread => {
-            res.render('show', {
-                bread: foundBread
-            })
-        })
-        .catch(err => {
-          res.render('404')
-        })
-});
-
-// Edit
-breads.get('/:indexArray/edit', (req, res) => {
-    res.render('edit', {
-        bread: Bread[req.params.indexArray],
-        index: req.params.indexArray
-    })
-});
-
 // Create
 breads.post('/', (req, res) => {
     if (!req.body.image) {
@@ -68,10 +30,48 @@ breads.post('/', (req, res) => {
     res.redirect('/breads'); // It takes the user back the index page
 });
 
+// New
+breads.get('/new', (req, res) => {
+    res.render('new')
+});
+
+// Show
+breads.get('/:id', (req, res) => {
+    Bread.findById(req.params.id)
+        .then(foundBread => {
+            res.render('show', {
+                bread: foundBread
+            })
+        })
+        .catch(err => {
+          res.render('404')
+        })
+});
+
+// Update
+breads.put('/:arrayIndex', (req, res) => {
+    if (req.body.hasGluten === 'on') {
+        req.body.hasGluten = true
+    } else {
+        req.body.hasGluten = false
+    };
+
+    Bread[req.params.arrayIndex] = req.body;
+    res.redirect(`/breads/${req.params.arrayIndex}`);
+});
+
 // Delete
 breads.delete('/:indexArray', (req, res) => {
     Bread.splice(req.params.indexArray, 1);
     res.status(303).redirect('/breads');
+});
+
+// Edit
+breads.get('/:indexArray/edit', (req, res) => {
+    res.render('edit', {
+        bread: Bread[req.params.indexArray],
+        index: req.params.indexArray
+    })
 });
 
 module.exports = breads;
