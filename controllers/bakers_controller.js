adb6d5b271ec77fb2959338b864cac1ee0ca6cfe
kiqueangulo <kiqueangulo@gmail.com>
2022-05-21 13:55:08 -0400

    Creating virtual field

--- controllers/bakers_controller.js
+++ controllers/bakers_controller.js
@@ -3,6 +3,16 @@ const baker = express.Router();
 const Baker = require('../models/bakers.js');
 const bakerSeedData = require('../models/bakers_seed.js');
 
+// Index
+baker.get('/', (req, res) => {
+    Baker.find()
+        .populate('breads')
+        .then(foundBakers => {
+            console.log(foundBakers);
+            res.send(foundBakers)
+        })
+});
+
 baker.get('/data/seed', (req, res) => {
     Baker.insertMany(bakerSeedData)
         .then(res.redirect('/breads'))
--- models/bakers.js
+++ models/bakers.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const Bread = require('./breads.js');
 const { Schema } = mongoose;
 
 const bakerSchema = new Schema({
@@ -12,6 +13,13 @@ const bakerSchema = new Schema({
         required: true
     },
     bio: String
+}, { toJSON: { virtuals: true } });
+
+// Virtual
+bakerSchema.virtual('breads', {
+    ref: 'Bread',
+    localField: '_id',
+    foreignField: 'baker'
 });
 
 module.exports = mongoose.model('Baker', bakerSchema);
