212462ab9aa6ed8a76c7aa3214fe13200721e0d3
kiqueangulo <kiqueangulo@gmail.com>
2022-08-02 21:21:23 -0400

    Fixing render

--- models/breads.js
+++ models/breads.js
@@ -1,30 +1,32 @@
-// require mongoose 
-const mongoose = require('mongoose');
-// creating shorthand for the Schema constructor 
+// require mongoose
+const mongoose = require("mongoose");
+// creating shorthand for the Schema constructor
 const { Schema } = mongoose;
 
 // Schema
 const breadSchema = new Schema({
-    name: { 
-        type: String, 
-        required: true 
-    },
-    hasGluten: Boolean,
-    image: { 
-        type: String, 
-        default: 'http://placekitten.com/400/400' 
-    },
-    baker: {
-        type: Schema.Types.ObjectId,
-        ref: 'Baker'
-    }
+  name: {
+    type: String,
+    required: true,
+  },
+  hasGluten: Boolean,
+  image: {
+    type: String,
+    default: "http://placekitten.com/400/400",
+  },
+  baker: {
+    type: Schema.Types.ObjectId,
+    ref: "Baker",
+  },
 });
 
-// helper methods 
-breadSchema.methods.getBakedBy = function(){
-    return `${this.name} was baked with love by ${this.baker.name}, who has been with us since ${this.baker.startDate.getFullYear()}.`
+// helper methods
+breadSchema.methods.getBakedBy = function () {
+  return `${this.name} was baked with love by ${
+    this.baker.name
+  }, who has been with us since ${this.baker.startDate.getFullYear()}.`;
 };
 
 // Model and export
-const Bread = mongoose.model('Bread', breadSchema);
+const Bread = mongoose.model("Bread", breadSchema);
 module.exports = Bread;
